3ef562258a34d35be144afaf8666a02baa86c4d8
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-03-12 12:33:49 -0300

    refactor(service): Adding login function

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private httpCliente: HttpClient
+  ) { }
+
+  login(name: string, password: string){
+    return this.httpCliente.post<LoginResponse>("/login", {name,password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token);
+        sessionStorage.setItem("username", value.name);
+      })
+    )
+  }
 }
