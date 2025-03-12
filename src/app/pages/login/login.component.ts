216ee63d33a5c8e06d839f76bc1aa4d6c390bb26
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-03-12 13:49:07 -0300

    refactor(interface): Adding loginForm interface

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,10 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
+interface LoginForm{
+  email: FormControl,
+  password: FormControl
+}
 @Component({
   selector: 'app-login',
   imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
@@ -17,7 +21,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginComponent {
 
-  loginForm!: FormGroup;
+  loginForm!: FormGroup<LoginForm>;
 
   constructor(
     private router: Router,
